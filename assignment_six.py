10fc69d440d4a684a6aad71c786ad8e8f0b7e503
anthonyfagiolo <fagioan@ssfs.org>
2021-11-04 15:15:23 -0400

    6 assignment incomplete

--- assignment_six.py
+++ assignment_six.py
@@ -9,8 +9,21 @@ def get_birthdays():
 
 def is_duplicates(list):
     for x in range(22):
-        list[x]
         for y in range (x+1,23):
-            list[y]
-        if list[x]==list[y]:
-            return True
+            if list[x]==list[y]:
+                return True
+    else:
+        return False
+
+def main ():
+    number = int(input("How many more times would you like to run the program? "))
+    sum = 0
+    for x in range (number):
+        list = get_birthdays()
+        is_duplicates(list)
+        if is_duplicates(list) == True:
+            sum += 1
+    print(sum/number)
+
+if __name__ == '__main__':
+    main()
